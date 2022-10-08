51a9eb9d137c67ba960684b8cf32e3015b0c05ce
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-10-08 18:14:39 +0900

    refactor: 解法に準拠

--- mycode/01_a04.py
+++ mycode/01_a04.py
@@ -1,17 +1,9 @@
 def main():
     n = int(input())
 
-    n_digit = list("0000000000")
-    i = len(n_digit) - 1
-
-    if n != 0:
-        while n > 1:
-            n_digit[i] = str(n % 2)
-            n //= 2
-            i -= 1
-        n_digit[i] = "1"
-
-    print(*n_digit, sep="")
+    for i in list(range(9))[::-1]:
+        wari = 1 << i  # 2のx乗
+        print((n // wari) % 2, end="")
 
 
 if __name__ == "__main__":
