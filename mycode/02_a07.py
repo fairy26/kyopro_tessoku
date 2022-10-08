1e2564889a6e6f7b30fd695e5796cb3d127bbefb
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-10-08 19:05:43 +0900

    fix: TLEのためO(N+D)の解放に修正

--- mycode/02_a07.py
+++ mycode/02_a07.py
@@ -1,15 +1,16 @@
 def main():
     d = int(input())
     n = int(input())
-    total = [0] * d
+    changes = [0] * (d + 1)
     for _ in range(n):
-        l, r = map(int, input().split())
-        for i in range(l, r + 1):
-            idx = i - 1
-            total[idx] += 1
+        l, r = map(int, input().split())  # l日~r日 → l日: +1, r+1日: -1
+        changes[l - 1] += 1
+        changes[r] -= 1
 
-    for num in total:
-        print(num)
+    total = 0
+    for i in range(d):
+        total += changes[i]
+        print(total)
 
 
 if __name__ == "__main__":
