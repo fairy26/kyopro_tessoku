5c4892a049dbb235639814086e761a8e0a8601e8
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-10-08 18:21:32 +0900

    add: 問題A05を追加

---
+++ mycode/01_a05.py
@@ -0,0 +1,12 @@
+def main():
+    n, k = map(int, input().split())
+    cnt = 0
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if 1 <= k - (i + j) <= n:
+                cnt += 1
+    print(cnt)
+
+
+if __name__ == "__main__":
+    main()
