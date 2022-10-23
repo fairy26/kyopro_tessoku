d51b34f8fc82ba7253a4c61c7214212144ec00dc
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-10-23 15:12:46 +0900

    change: ライブラリを使用するよう変更

--- mycode/03_a11.py
+++ mycode/03_a11.py
@@ -1,22 +1,11 @@
+from bisect import bisect_right
+
+
 def main():
     n, x = map(int, input().split())  # xは必ずaの要素である
     a = list(map(int, input().split()))
 
-    a.sort()
-
-    i_left, i_right = 0, n
-
-    while i_left <= i_right:
-        i = (i_left + i_right) // 2
-
-        if x == a[i]:
-            break
-        if x > a[i]:
-            i_left = i + 1
-        else:
-            i_right = i - 1
-
-    print(i + 1)  # 問題の添字 = index + 1
+    print(bisect_right(sorted(a), x))
 
 
 if __name__ == "__main__":
